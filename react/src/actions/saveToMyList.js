2e3f8f08ef1f3f6c4d56b0255a5c55682ab8ad99
Lance Whitesel <lance@eucleo.com>
2018-04-06 13:23:18 -0400

    Added some code that updates the song cache after adding a song.  This approach is as opposed to just re-running the query.  This saves lots of network traffic and is not that hard to do.  I'm liking GraphQL & Apollo more and more.

--- react/src/actions/saveToMyList.js
+++ react/src/actions/saveToMyList.js
@@ -1,6 +1,7 @@
 import { SAVE_TO_MY_LIST } from '.';
 import { apolloClient } from '../app';
 import addSong from './mutations/addSong';
+import fetchSongs from './queries/fetchSongs';
 
 async function saveSong(song) {
   const newSong = await apolloClient.mutate({
@@ -12,6 +13,15 @@ async function saveSong(song) {
       imageUrl: song.imageUrl,
     },
   }).then(({ data }) => {
+    const cache = apolloClient.readQuery({
+      query: fetchSongs,
+    });
+    cache.songList.push(data.addSong);
+    apolloClient.writeQuery({
+      query: fetchSongs,
+      data: cache,
+    });
+
     const addedSong = data.addSong;
 
     return {
--- react/src/store/songListStore.js
+++ react/src/store/songListStore.js
@@ -1,11 +1,12 @@
 import ReduxPromise from 'redux-promise';
+import ReduxLogger from 'redux-logger';
 import thunk from 'redux-thunk';
 import { compose, createStore, applyMiddleware } from 'redux';
 
 import rootReducer from '../reducers';
 
 export default (initialState, debug = false) => {
-  const middleware = applyMiddleware(ReduxPromise, thunk);
+  const middleware = applyMiddleware(ReduxPromise, ReduxLogger, thunk);
   const createStoreWithMiddleware = compose(middleware);
 
   return createStoreWithMiddleware(createStore)(rootReducer, initialState);
